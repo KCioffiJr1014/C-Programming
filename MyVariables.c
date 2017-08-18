a7b3d65f81da48170381ec9a2a14cf9535e7c3cc
KCioffiJr1014 <KCioffiJr1014@users.noreply.github.com>
2017-08-18 02:55:06 +0000

    First commit
    Beginning to learn C

---
+++ MyVariables.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+void main() 
+{
+
+	int age = 21;
+	int sum = 1 + 2 + 3 +4;
+	int avg = sum /4;
+	float avg2 = sum / 4.0f;
+	printf("My age is %d\n", age);
+	printf("Sum of 4 numbers is %d\n", sum);
+	printf("The Average of our sum is equal to %d\n", avg);
+	printf("float Avg = %f\n", avg2);
+}
